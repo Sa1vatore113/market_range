122d990021a4efa16dbed0ef58c9d074857dcc8a
Sa1vatore113 <jekas113@gmail.com>
2025-09-28 12:35:08 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    GIST_ID: 'd69178844555b679725326d4174f0b7a',
+    GITHUB_TOKEN: '', // Замените на реальный токен
+    GITHUB_USERNAME: 'Sa1vatore113',
+    SYNC_ENABLED: true
+};
